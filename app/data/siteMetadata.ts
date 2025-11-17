91d4b78643a9d62be08d9d5b8fa4506487b1fe2e
Manish-Tamang <maneshtamang833@gmail.com>
2025-11-17 14:34:31 +0545

    Add site metadata configuration

---
+++ app/data/siteMetadata.ts
@@ -0,0 +1,8 @@
+export const siteMetadata = {
+  title: "Hack Sushma",
+  description:
+    "Join 20+ teams of passionate developers for an epic 12-hour coding marathon! December 3, 2024 • 3-day session of innovation, collaboration, and code.",
+  language: "en-us",
+  siteUrl: "https://hacksushma.xyz",
+  locale: "en-US",
+};
