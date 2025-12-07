7ed07eb6a00b480da21a28f81107fb15c67a44f1
Manish-Tamang <maneshtamang833@gmail.com>
2025-12-08 01:55:38 +0545

    feat: simplify RootLayout by removing unused components and improving structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "app/components/Navbar";
-import { BgGradient } from "./components/BgGradient";
-import { ThemeToggle } from "./components/ThemeToggle";
-import { cx } from "./lib/utils";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
@@ -40,20 +36,8 @@ export default function RootLayout({
       lang="en"
       className={`bg-bg-primary dark:bg-neutral-900 ${GeistMono.variable} ${GeistSans.variable}`}
     >
-      <body className="flex min-h-screen flex-col font-sans max-w-full md:max-w-[880px] overflow-x-hidden md:overflow-y-auto lg:mx-auto bg-bg-primary dark:bg-neutral-900 transition-colors">
-
-        <main className={cx("relative flex flex-1 flex-col min-h-0 max-w-full bg-bg-primary dark:bg-neutral-900")}>
-          <Navbar />
-          <div className="grid flex-1 grid-cols-1 lg:grid-cols-[32px_1fr_32px] min-h-0 bg-bg-primary dark:bg-neutral-900">
-            <div className="hidden w-full border-r border-border-primary dark:border-neutral-700 opacity-75 lg:block bg-bg-primary dark:bg-neutral-900"></div>
-            <div className="relative col-span-1 px-3 lg:px-0 min-h-0 bg-bg-primary dark:bg-neutral-900">
-              <BgGradient />
-              {children}
-            </div>
-            <div className="hidden w-full border-l border-border-primary dark:border-neutral-700 opacity-75 lg:block bg-bg-primary dark:bg-neutral-900"></div>
-          </div>
-          {/* <ThemeToggle /> */}
-        </main>
+      <body className="flex min-h-screen flex-col font-sans bg-bg-primary dark:bg-neutral-900 transition-colors">
+        {children}
       </body>
     </html>
   );
