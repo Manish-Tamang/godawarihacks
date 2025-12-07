543796d541f4995b0b4dd50000a1e9b7b91bc445
Manish-Tamang <maneshtamang833@gmail.com>
2025-12-08 01:21:41 +0545

    feat: update BgGradient component for improved layout responsiveness

--- app/components/BgGradient.tsx
+++ app/components/BgGradient.tsx
@@ -6,7 +6,7 @@ export function BgGradient() {
           "linear-gradient(to bottom, #08475000 0%, #084750 50%, #08475000 100%)",
         opacity: "6%",
       }}
-      className="pointer-events-none absolute left-1/2 top-0 -z-10 h-screen w-[840px] -translate-x-1/2 overflow-hidden blur-3xl"
+      className="pointer-events-none absolute left-1/2 top-0 -z-10 h-screen w-full max-w-[840px] -translate-x-1/2 overflow-hidden blur-3xl"
     ></div>
   );
 }
