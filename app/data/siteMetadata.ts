14df43721687ea275649cdf65606c14cb73a6d37
Manish-Tamang <maneshtamang833@gmail.com>
2025-12-17 23:41:42 +0545

    fix: Update title from "Godawari Hack" to "Godawari Hacks" in siteMetadata

--- app/data/siteMetadata.ts
+++ app/data/siteMetadata.ts
@@ -1,5 +1,5 @@
 export const siteMetadata = {
-  title: "Godawari Hack",
+  title: "Godawari Hacks",
   description:
     "Join 30+ teams of passionate developers for an epic 24-hour coding marathon!",
   language: "en-us",
