d2622f8fdb886b640b7e5787861f3eb6609bc5e5
Manish-Tamang <maneshtamang833@gmail.com>
2025-12-17 23:41:48 +0545

    fix: Update title and description from "Godawari hack" to "Godawari Hacks" in RegisterLayout component

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Register - Godawari hack",
-  description: "Register your team of 4 members for Godawari hack. Join us for an epic 24-hour coding marathon!",
+  title: "Register - Godawari Hacks",
+  description: "Register your team of 4 members for Godawari Hacks. Join us for an epic 24-hour coding marathon!",
 };
 
 export default function RegisterLayout({
